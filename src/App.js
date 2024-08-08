4618b4f538eb21bc83d77d8104e831edeec9ec73
Ranjeet Vishwakarma <rv238502@gmail.com>
2024-08-08 11:26:43 +0530

    Added the useContext folder with two sub-files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 // import UseState from "./component/UseState.tsx";
 // import UseEffect from "./component/UseEffect.tsx"
-import MyComponent from "./component/MyComponent.tsx";
+// import MyComponent from "./component/MyComponent.tsx";
+
+import FunctionContextComponent from "./component/useContext/FunctionContextComponent.tsx";
+import ClassContextComponent from "./component/useContext/ClassContextComponent.tsx";
+export const ThemeContext = createContext();
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  function toggleTheme() {
+    setDarkTheme((preDarkTheme) => !preDarkTheme);
+  }
+
   return (
     <>
-      <h1>React Hook Example By WebDevSimplified </h1>
+      {/* <h1>React Hook Example By WebDevSimplified </h1> */}
       {/* <UseState /> */}
       {/* <UseEffect /> */}
-      <MyComponent></MyComponent>
+      {/* <MyComponent></MyComponent> */}
+
+      <ThemeContext.Provider value={darkTheme}>
+        <button type="button" onClick={toggleTheme}>
+          Toggle Theme
+        </button>
+        <FunctionContextComponent />
+        <ClassContextComponent />
+      </ThemeContext.Provider>
     </>
   );
 };
